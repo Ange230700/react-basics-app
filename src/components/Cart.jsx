de2c58a4984a54d866218a749ac147e709c4f90f
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-10 16:54:02 +0200

    working... part 2

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,7 @@
-function Cart(props) {
-    console.log(props);
-    return `You bought ${props.product.quantity} ${props.product.name} for a total of €${props.calculate(props.product)}`;
+function Cart({ product, calculate }) {
+    // console.log(props);
+    // const { product, calculate } = props; 
+    return `You bought ${product.quantity} ${product.name} for a total of €${calculate(product)}`;
 }
 
 export default Cart;
