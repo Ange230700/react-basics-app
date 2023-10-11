a32a8a2a68427c904fe2ca429837a4d2b679935a
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-11 16:18:19 +0200

    Just passing props...

--- src/components/DisplayMessage.jsx
+++ src/components/DisplayMessage.jsx
@@ -1,5 +1,11 @@
+import PropTypes from 'prop-types';
+
 function DisplayMessage({ message }) {
     return <p>{message}</p>
+}
+
+DisplayMessage.propTypes = {
+    message: PropTypes.string.isRequired
 }
 
 export default DisplayMessage;
--- src/components/InputMessage.jsx
+++ src/components/InputMessage.jsx
@@ -1,8 +1,16 @@
+import PropTypes from 'prop-types';
+
 function InputMessage({setMessage}) {
     function handleChange(e) {
         setMessage(e.target.value);
     }
     return <input name='message' onChange={handleChange} />
 }
+
+InputMessage.propTypes = {
+    setMessage: PropTypes.func.isRequired
+}
+
+// ? When do we use the shape method for PropTypes?
 
 export default InputMessage;
