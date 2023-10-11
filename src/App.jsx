4f13704e0f102c9ebbbcc41ebaa59651b3827983
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-11 10:40:53 +0200

    looking at what a useState hook returns

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import './App.css';
+import { useState } from 'react';
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css';
 // import Cart from './components/Cart';
-import SayHello from './components/SayHello';
+// import SayHello from './components/SayHello';
 
 function App() {
   // const product = {
@@ -15,13 +16,20 @@ function App() {
   // const calculate = (product) => {
   //   return product.quantity * product.price;
   // };
-  
+
   // return (
   //   <>
   //     <Cart product={product} calculate={calculate} />
   //   </>
   // )
-  return <SayHello name="Wilder" age={22}/>
+  // const wilder = {
+  //   name: 'Wildo',
+  //   languages: ['HTML', 'CSS', 'JavaScript', 'React']
+  // };
+  // return <SayHello wilder={wilder} />
+  const state = useState();
+  console.log(state);
+  return <p>Hello World!</p>
 }
 
 export default App
