25bd321ea7d151a4670873d3066bcd2f4524aeda
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-11 11:04:48 +0200

    Placing an event listener on a button

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,10 @@
 import './App.css';
-import { useState } from 'react';
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import Cart from './components/Cart';
-// import SayHello from './components/SayHello';
 
 function App() {
-  // const product = {
-  //   name: 'iPhone',
-  //   quantity: 2,
-  //   price: 999
-  // };
-
-  // const calculate = (product) => {
-  //   return product.quantity * product.price;
-  // };
-
-  // return (
-  //   <>
-  //     <Cart product={product} calculate={calculate} />
-  //   </>
-  // )
-  // const wilder = {
-  //   name: 'Wildo',
-  //   languages: ['HTML', 'CSS', 'JavaScript', 'React']
-  // };
-  // return <SayHello wilder={wilder} />
-  const [count, setCount] = useState(0);
-  console.log(count);
-  return <p>Hello World!</p>
+  const handleClick = (event) => {
+    console.log(event.target);
+  }
+  return <button onClick={handleClick}>Click</button>
 }
 
 export default App
